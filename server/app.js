cb164355f7c17d8465557cfa17a792a74df19eb0
Fahad Hossain <evilinfinite7@gmail.com>
2017-05-19 17:14:04 +0600

    Re-write the server

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const Express = require('express')
+const download = require('download')
+const baby = require('babyparse')
+const ssize = require('lodash.samplesize')
+const find = require('lodash.find')
+const { parseFragment } = require('parse5')
+const app = new Express()
+let rawData
+let opDone = false
+
+app.get('/json', function (req, res) {
+  if (opDone) {
+    const links = ssize(rawData, 10).map(
+      el => find(parseFragment(el).childNodes[0].attrs, ['name', 'src']).value
+    )
+    res.send(
+      JSON.stringify({
+        message: 'Successful',
+        data: links
+      })
+    )
+  } else {
+    res.status(404).send({
+      message: 'No JSON data available right now. Reload in a minute or two.'
+    })
+  }
+})
+
+function startDownload () {
+  download(
+    'https://spankbang.com/static_desktop/CSV/spankbang.24hr.zip',
+    path.resolve(__dirname),
+    { extract: true }
+  ).then(() => {
+    rawData = baby.parseFiles('spankbang.24hr.csv').data.map(r => r[0])
+    opDone = true
+  })
+}
+
+app.listen(3030, 'localhost', function () {
+  console.log('Express is listening on port 3030')
+  startDownload()
+})
